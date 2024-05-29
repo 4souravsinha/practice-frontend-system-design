d15d2e33e47650c40439943053d60199e3c59f0f
Sourav Sinha <4souravsinhain@gmail.com>
2024-05-29 11:39:05 +0530

    code formating

--- star-rating/src/components/star-rating.jsx
+++ star-rating/src/components/star-rating.jsx
@@ -1,52 +1,44 @@
 import { useState } from "react";
 
-const StarRating = ({size=5 , handleRatingChange=()=>{} , rating}) => {
+const StarRating = ({ size = 5, handleRatingChange = () => {}, rating }) => {
+  const [hoveredStar, setHoveredStar] = useState(0);
+  const [isHovering, setIsHovering] = useState(false);
 
-  const [hoveredStar , setHoveredStar] = useState(0);
-  const [isHovering , setIsHovering] = useState(false);
-
-  const handleHover = (starno)=>{
+  const handleHover = (starno) => {
     setHoveredStar(starno);
     setIsHovering(true);
-  } 
-  
+  };
+
   const handleRemoveHover = () => {
     setHoveredStar(0);
     setIsHovering(false);
-  }
+  };
 
   const stars = Array(size).fill("");
- 
 
-  return (
+  return stars.map((_, index) => {
+    let classes = "single-star";
+    let starno = index + 1;
 
-       stars.map((_ , index)=>{
+    if (starno <= hoveredStar) {
+      classes += " hover-star";
+    }
+    if (starno <= rating && !isHovering) {
+      classes += " selected-star";
+    }
 
-        let classes = "single-star";
-        let starno = index + 1;
-        
-        if(starno <= hoveredStar){
-          classes += ' hover-star'
-        }
-        if(starno <= rating && !isHovering){
-          classes += ' selected-star'
-        }
+    return (
+      <span
+        key={index}
+        className={classes}
+        onMouseEnter={() => handleHover(starno)}
+        onMouseLeave={handleRemoveHover}
+        onClick={() => handleRatingChange(starno)}
+      >
+        {"★"}
+      </span>
+    );
+  });
+};
 
-        return (
-          <span
-            key={index}
-            className={classes}
-            onMouseEnter={()=>handleHover(starno)}
-            onMouseLeave={handleRemoveHover}
-            onClick={()=>handleRatingChange(starno)}
-          >
-            {"★"}
-          </span>
-        );
-
-       })
-    
-  )
-}
-
-export default StarRating
+export default StarRating;
